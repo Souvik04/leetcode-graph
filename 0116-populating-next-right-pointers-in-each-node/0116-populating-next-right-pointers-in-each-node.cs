7c750515aefba6b9bf707b326a0e09d6f4bfcb37
Souvik Ghosh <g.souvik04@gmail.com>
2024-10-26 18:27:01 +0530

    Time: 92 ms (11.11%), Space: 47.1 MB (29%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cs
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+public class Solution {
+    public Node Connect(Node root) {
+        if(root == null){
+            return root;
+        }
+
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+
+        while(queue.Count > 0){
+            int level = queue.Count;
+            Node prev = null;            
+
+            for(int i = 0; i < level; i++){
+                Node cur = queue.Dequeue();
+
+                if(prev != null){
+                    prev.next = cur;
+                }
+
+                prev = cur;
+
+                if(cur.left != null){
+                    queue.Enqueue(cur.left);
+                }
+
+                if(cur.right != null){
+                    queue.Enqueue(cur.right);
+                }
+            }
+
+            prev.next = null;
+        }
+        return root;
+    }
+}
